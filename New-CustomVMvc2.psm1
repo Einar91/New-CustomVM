1def1127295a7266425fe39c24b4d3ec6fc7ff70
Einar Kristiansen <einarr.kristiansen@outlook.com>
2019-05-21 15:26:50 +0200

    Added section for portgroup selection

--- New-CustomVMvc2.psm1
+++ New-CustomVMvc2.psm1
@@ -15,9 +15,9 @@ New-CustomVMvc2 -VMName SRV1 -ViServer Vcenter.lab.no -ServerHost NameOfHost -Po
 .EXAMPLE
 New-CustomVMvc2 -VMName SRV1 -ViServer Vcenter.lab.no -SelectHostBy FreeSpaceGB -Location 'Discovered virtual machine'
 .EXAMPLE
-New-CustomVMvc2 -VMName SRV1 -ViServer Vcenter.lab.no -DiskStorageFormat Thick -DiskGB "60","8" -ScsiType ParaVirtual -Floppy -CD
+New-CustomVMvc2 -VMName SRV1 -ViServer Vcenter.lab.no -Datastore 'EinarStorage' -DiskStorageFormat Thick -DiskGB "60","8" -ScsiType ParaVirtual -Floppy -CD
 .EXAMPLE
-New-CustomVMvc2 -VMName SRV1 -ViServer Vcenter.lab.no -NumCpu 4 -CoresPerSocket 2 -MemoryGB 4 -NetAdapterType Vmxnet3 -CheckHostConnectionState -LocationByVMName
+New-CustomVMvc2 -VMName SRV1 -ViServer Vcenter.lab.no -NumCpu 4 -CoresPerSocket 2 -MemoryGB 4 -NetAdapterType Vmxnet3 -CheckHostConnectionState -HostByVMName
 #>
 
 function New-CustomVMvc2 {
@@ -63,6 +63,10 @@ function New-CustomVMvc2 {
 
     [Parameter(Mandatory=$false,
         ValueFromPipelineByPropertyName=$True)]
+    [string]$Datastore,
+
+    [Parameter(Mandatory=$false,
+        ValueFromPipelineByPropertyName=$True)]
     [ValidateSet('Thick','Example')]
     [string]$DiskStorageFormat = 'Thick',
 
@@ -102,11 +106,15 @@ function New-CustomVMvc2 {
 
     [Parameter(Mandatory=$false)]
     [Switch]
-    $LocationByVMName,
+    $HostByVMName,
 
     [Parameter(Mandatory=$false)]
     [Switch]
-    $CheckHostConnactionState
+    $CheckHostConnactionState,
+
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $ConfirmConfiguration
     )
 
 BEGIN {
@@ -117,7 +125,76 @@ BEGIN {
 
 PROCESS {
     foreach($NewVM in $VMName){
+        Try{
+            #Check if we want to define host for VM by name
+            if($PSBoundParameters.ContainsKey('HostByVMName')){
+                Write-Verbose -Message 'Setting sitename by vm name.'
+                $SiteName = ($NewVM.Substring(0,3)).ToUpper()
+            
+                Write-Verbose -Message "Searching avilable host for $SiteName"
+                $ValidHosts = Get-VMHost -Name $SiteName*
 
+                #If no valid hosts, write warning and abort.
+                if(!$ValidHosts){
+                    Write-Warning -Message "Can not automatically find any available VM Hosts for $SiteName"
+                    Write-Error -Message "No VM Host for $SiteName found" -ErrorAction Stop
+                }
+
+                if(($ValidHosts.count) -eq 1){
+                    $ServerHost = $ValidHosts
+                } #If
+                
+                if(($ValidHosts.count) -gt 1 ){
+                    Write-Verbose -Message "Selecting host by $SelectHostBy"
+                    $ValidHostsChoices = @()
+                    foreach($VMHost in $ValidHosts){
+                        #Calculate free mem, cpu and storage
+                        $FreeCPU = ($VMHost.CpuTotalMhz) - ($VMHost.CpuUsageMhz)
+                        $FreeMemory = ($VMHost.MemoryTotalGB) - ($VMHost.MemoryUsageGB)
+                        $FreeStorage = $VMHost | Get-Datastore | Select-Object -ExpandProperty FreeSpaceGB
+
+                        #Create a new object with our properties
+                        $ValidHostsProperties = @{'Name'=$VMHost.Name
+                                                'FreeCPU'=$FreeCPU
+                                                'FreeMemoryGB'=$FreeMemory
+                                                'FreeSpaceGB'=$FreeStorage}
+                        $ValidHostsChoices += New-Object psobject -Property $ValidHostsProperties    
+                    } #Foreach VMHost
+                
+                    #Select VM based on parameter $SelectByHost
+                    $ServerHost = $ValidHostsChoices |
+                        Sort-Object $SelectHostBy -Descending |
+                        Select-Object -First 1 -ExpandProperty Name
+
+                    #Get full host object
+                    $ServerHost = Get-VMHost -Name $ServerHost
+                } #If ($ValidHosts.count) -gt 1
+            } #If $PSBoundParameters.ContainsKey('HostByVMName')
+
+            #Select portgroup if not defined by parameter
+            If($PSBoundParameters.ContainsKey('Portgroup') -eq $false){
+                Write-Verbose -Message "Selecting VirtualPortGroup from $($ServerHost.Name)"
+                $Portgroup = $ServerHost |
+                    Get-VirtualPortGroup -Name $SiteName* |
+                    Select-Object -First 1
+                
+                if(!$Portgroup){
+                    Write-Warning -Message "No SiteName portgroup found for $SiteName"
+                    Write-Verbose -Message "Searching for alternative portgroup on $($ServerHost.Name)"
+                    $Portgroup = $ServerHost |
+                        Get-VirtualPortGroup |
+                        Sort-Object VLandId -Descending |
+                        Select-Object -First 1
+                } #If
+            } #If $PSBoundParameters.ContainsKey('Portgroup')
+            
+                
+            }
+
+        } #Try
+        Catch{
+
+        } #Catch
     } #Foreach $vmname
 } #Process
 
